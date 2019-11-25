fb3db025529519b06cbfad77bfcc7df9cf30ae08
S0LERA <pablo.rodriguez22@alu.uclm.es>
2019-11-25 13:38:35 +0100

    Versión 0.9

--- secuencial.c
+++ secuencial.c
@@ -4,6 +4,7 @@
 #include "timer.h"
 #include <limits.h>
 
+//Estructuras
 typedef struct {
     int* pobl;
     int cont;
@@ -19,27 +20,56 @@ typedef struct {
 
 typedef stack_struct *mystack;
 
-void push(stack_struct stack, tour_struct tour){
-    
+//Variables Globales
+tour_t besttour;
+mystack stack = NULL;
+int **digraph;
+
+//Métodos
+void push(tour_t tour){
+    stack->list[stack->list_sz] = tour;
+    stack->list_sz = stack->list_sz + 1;
+}
+
+void pop(tour_t tour){
+    stack->list = stack->list_sz - 1;
+    tour = stack->list[stack->list_sz];
 }
 
 
-Rec_en_profund(tour_t *tour, int poblaciones[]){
-    
+void Rec_en_profund(tour_t tour, int n){
+    tour = (0,1,0);
+    push(tour);
+    while(stack->list_sz != 0){
+        pop(tour);
+        if(tour->cont == n){
+            if(tour->coste < besttour->coste){
+                besttour = tour;
+            }
+        }else{
+            for(int i = n-1; i==0; i--){
+                if((tour->coste + digraph[tour->pobl[tour->cont]][i])< besttour->coste){
+                    tour->pobl[tour->cont+1] = i;
+                    tour->coste = tour->coste + digraph[tour->pobl[tour->cont]][i];
+                    push(tour);
+                    tour->coste = tour->coste - digraph[tour->pobl[tour->cont]][i];
+                    tour->pobl[tour->cont+1] = 0;
+                }
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[]){
     double inicio, fin;
     int i, j, n;
-    int **digraph;
-    tour_t tour, besttour;
     FILE *digraph_file;
+    tour_t tour;
+
+    //Leemos el fichero y lo pasamos a la variable digraph
     digraph_file = fopen(argv[1], "r");
     fscanf(digraph_file, "%d,", &n);
-    int poblaciones[n];
-    for(i=0; i<n;i++){
-        poblaciones[i]=i;
-    }
+
     //Reservamos memoria para el digraph
     digraph = (int**)malloc(n*sizeof(int*));
     for(i=0;i<n;i++){
@@ -50,15 +80,15 @@ int main(int argc, char *argv[]){
         for(j=0;j<n;j++){
             fscanf(digraph_file, "%d,", &digraph[i][j]);
         }
-    }
+    } 
     //Reservamos espacio para tour y besttour
-    tour = malloc(sizeof(tour_t));
-    besttour = malloc(sizeof(tour_t));
+    tour = malloc((n+1)*sizeof(tour_t));
+    besttour = malloc((n+1)*sizeof(tour_t));
 
-    tour = (0,1,0);
-    besttour = (0,1, INT_MAX);
+    tour = (0,0,0);
+    besttour = (0,0, INT_MAX);
 
     GET_TIME(inicio);
-    Rec_en_profund(tour, &poblaciones);
+    Rec_en_profund(tour,n);
     GET_TIME(fin);
 }
